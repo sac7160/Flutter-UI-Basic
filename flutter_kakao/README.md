a7a722fdc181f033461c47dfed9152f8cacc838c
Chang Hyeon Park <sac7160@naver.com>
2022-01-15 15:53:12 +0900

    flutter chatApp UI

---
+++ flutter_kakao/README.md
@@ -0,0 +1,7 @@
+# 1. 주요 내용
+* pubspec.yaml 파일지  (Font_awesome_flutter 패키지, intl 패키지)
+* IndexdStack 위젯
+* BottomNavigationBar 위젯
+* ListTile - 양측의 아이콘과 가운데 1~3줄의 텍스트로 구성되어 있는 위젯
+* List.generate()
+* RoundIconButton 위젯 - 아이콘 설정후 재사용하기
---
+++ flutter_kakao/lib/screens/main_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/chat_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/more_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/foundation.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendScreen(),
+          ChatScreen(),
+          MoreScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
+        ],
+      ),
+    );
+  }
+}
