d451c2a299430f504321619a5a944eb0408ae515
Chang Hyeon Park <sac7160@naver.com>
2022-01-16 16:34:43 +0900

    flutter chatApp UI

--- flutter_kakao/README.md
+++ flutter_kakao/README.md
@@ -5,3 +5,7 @@
 * ListTile - 양측의 아이콘과 가운데 1~3줄의 텍스트로 구성되어 있는 위젯
 * List.generate()
 * RoundIconButton 위젯 - 아이콘 설정후 재사용하기
+* Flexible 위젯
+* SingleChildScrollView
+* TextEditingController
+* GridView
--- flutter_kakao/lib/screens/chat_screen.dart
+++ flutter_kakao/lib/screens/chat_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/models/chat.dart';
+import 'package:flutter_kakao/components/chat_card.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Chat Screen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(centerTitle: false, title: Text("채팅")),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
     );
   }
 }
